b698dc1ec0a56addf3d881c3bc3f8b791c2ab722
peter <sfw811@hotmail.com>
2019-02-03 15:52:42 +0100

    check

--- semantic-segmentation/Models/Segnet.py
+++ semantic-segmentation/Models/Segnet.py
@@ -10,14 +10,14 @@ from keras.layers.convolutional import Convolution2D
 
 from mylayers import MaxPoolingWithArgmax2D, MaxUnpooling2D
 
-def segnet_indices_pooling(nClasses, input_height=480, input_width=736, nchannel=3, pre_train = False):
+
+def segnet_indices_pooling(nClasses, input_height=480, input_width=736, nchannel=3, pre_train=False):
 	"""
-		debug:
-nClasses=12
-input_height=480
-input_width=736
-nchannel = 3
-	"""
+		nClasses=12
+		input_height=480
+		input_width=736
+		nchannel = 3
+		"""
 
 	input_shape = (input_height, input_width, nchannel)
 	kernel = 5
@@ -138,8 +138,8 @@ nchannel = 3
 	conv_26 = BatchNormalization()(conv_26)
 
 	conv_26 = Reshape(
-	(input_shape[0] * input_shape[1], nClasses),
-	input_shape=(input_shape[0], input_shape[1], nClasses))(conv_26)
+		(input_shape[0] * input_shape[1], nClasses),
+		input_shape=(input_shape[0], input_shape[1], nClasses))(conv_26)
 
 	outputs = Activation(output_mode)(conv_26)
 	print("Build decoder done..")
@@ -164,6 +164,7 @@ nchannel = 3
 
 	return model
 
+
 def segnet_level3(nClasses, input_height=500, input_width=750):
 	# nClasses =12
 	input_shape = (input_height, input_width, 5)
@@ -173,68 +174,68 @@ def segnet_level3(nClasses, input_height=500, input_width=750):
 	# ------------------------------------------------------------------------------------------------------------ #
 	# Encoder block[1]
 	inputs = Input(shape=input_shape)
-	conv_1 = Conv2D(filters=64, kernel_size=(3,3), strides=(1, 1), padding='same')(inputs)
+	conv_1 = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same')(inputs)
 	# 'channels_last' corresponds to inputs with shape (batch, height, width, channels)
 	conv_1 = BatchNormalization()(conv_1)
 	conv_1 = Activation('relu')(conv_1)
 
-	conv_2 = Conv2D(filters=64, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_1)
+	conv_2 = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_1)
 	conv_2 = BatchNormalization()(conv_2)
 	conv_2 = Activation('relu')(conv_2)
 
-	temp = MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid')(conv_2)
+	temp = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(conv_2)
 
 	# Encoder block[2]
-	conv_3 = Conv2D(filters=128, kernel_size=(3,3), strides=(1, 1), padding='same')(temp)
+	conv_3 = Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same')(temp)
 	conv_3 = BatchNormalization()(conv_3)
 	conv_3 = Activation('relu')(conv_3)
 
-	conv_4 = Conv2D(filters=128, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_3)
+	conv_4 = Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_3)
 	conv_4 = BatchNormalization()(conv_4)
 	conv_4 = Activation('relu')(conv_4)
 
 	temp = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(conv_4)
 
 	# Encoder block[3]
-	conv_5 = Conv2D(filters=256, kernel_size=(3,3), strides=(1, 1), padding='same')(temp)
+	conv_5 = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same')(temp)
 	conv_5 = BatchNormalization()(conv_5)
 	conv_5 = Activation('relu')(conv_5)
 
-	conv_6 = Conv2D(filters=256, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_5)
+	conv_6 = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_5)
 	conv_6 = BatchNormalization()(conv_6)
 	conv_6 = Activation('relu')(conv_6)
 
-	conv_7 = Conv2D(filters=256, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_6)
+	conv_7 = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_6)
 	conv_7 = BatchNormalization()(conv_7)
 	conv_7 = Activation('relu')(conv_7)
 
 	temp = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(conv_7)
 
 	# Encoder block[4]
-	conv_8 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(temp)
+	conv_8 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(temp)
 	conv_8 = BatchNormalization()(conv_8)
 	conv_8 = Activation('relu')(conv_8)
 
-	conv_9 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_8)
+	conv_9 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_8)
 	conv_9 = BatchNormalization()(conv_9)
 	conv_9 = Activation('relu')(conv_9)
 
-	conv_10 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_9)
+	conv_10 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_9)
 	conv_10 = BatchNormalization()(conv_10)
 	conv_10 = Activation('relu')(conv_10)
 
 	temp = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(conv_10)
 
 	# Encoder block[5]
-	conv_11 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(temp)
+	conv_11 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(temp)
 	conv_11 = BatchNormalization()(conv_11)
 	conv_11 = Activation('relu')(conv_11)
 
-	conv_12 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_11)
+	conv_12 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_11)
 	conv_12 = BatchNormalization()(conv_12)
 	conv_12 = Activation('relu')(conv_12)
 
-	conv_13 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_12)
+	conv_13 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_12)
 	conv_13 = BatchNormalization()(conv_13)
 	conv_13 = Activation('relu')(conv_13)
 
@@ -252,17 +253,17 @@ def segnet_level3(nClasses, input_height=500, input_width=750):
 	# ------------------------------------------------------------------------------------------------------------ #
 	# Decoder block[5]
 	temp = UpSampling2D(size=(2, 2))(temp)
-	temp = ZeroPadding2D(((0, 1), (0, 0)))(temp)  #  ((top_pad, bottom_pad), (left_pad, right_pad))
+	temp = ZeroPadding2D(((0, 1), (0, 0)))(temp)  # ((top_pad, bottom_pad), (left_pad, right_pad))
 
-	conv_14 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(temp)
+	conv_14 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(temp)
 	conv_14 = BatchNormalization()(conv_14)
 	conv_14 = Activation('relu')(conv_14)
 
-	conv_15 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_14)
+	conv_15 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_14)
 	conv_15 = BatchNormalization()(conv_15)
 	conv_15 = Activation('relu')(conv_15)
 
-	conv_16 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_15)
+	conv_16 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_15)
 	conv_16 = BatchNormalization()(conv_16)
 	conv_16 = Activation('relu')(conv_16)
 
@@ -270,15 +271,15 @@ def segnet_level3(nClasses, input_height=500, input_width=750):
 	temp = UpSampling2D(size=(2, 2))(conv_16)
 	temp = ZeroPadding2D(((0, 0), (0, 1)))(temp)
 
-	conv_17 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(temp)
+	conv_17 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(temp)
 	conv_17 = BatchNormalization()(conv_17)
 	conv_17 = Activation('relu')(conv_17)
 
-	conv_18 = Conv2D(filters=512, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_17)
+	conv_18 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_17)
 	conv_18 = BatchNormalization()(conv_18)
 	conv_18 = Activation('relu')(conv_18)
 
-	conv_19 = Conv2D(filters=256, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_18)
+	conv_19 = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_18)
 	conv_19 = BatchNormalization()(conv_19)
 	conv_19 = Activation('relu')(conv_19)
 
@@ -286,15 +287,15 @@ def segnet_level3(nClasses, input_height=500, input_width=750):
 	temp = UpSampling2D(size=(2, 2))(conv_19)
 	temp = ZeroPadding2D(((0, 1), (0, 1)))(temp)
 
-	conv_20 = Conv2D(filters=256, kernel_size=(3,3), strides=(1, 1), padding='same')(temp)
+	conv_20 = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same')(temp)
 	conv_20 = BatchNormalization()(conv_20)
 	conv_20 = Activation('relu')(conv_20)
 
-	conv_21 = Conv2D(filters=256, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_20)
+	conv_21 = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_20)
 	conv_21 = BatchNormalization()(conv_21)
 	conv_21 = Activation('relu')(conv_21)
 
-	conv_22 = Conv2D(filters=128, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_21)
+	conv_22 = Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_21)
 	conv_22 = BatchNormalization()(conv_22)
 	conv_22 = Activation('relu')(conv_22)
 
@@ -302,23 +303,22 @@ def segnet_level3(nClasses, input_height=500, input_width=750):
 	temp = UpSampling2D(size=(2, 2))(conv_22)
 	temp = ZeroPadding2D(((0, 0), (0, 1)))(temp)
 
-	conv_23 = Conv2D(filters=128, kernel_size=(3,3), strides=(1, 1), padding='same')(temp)
+	conv_23 = Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same')(temp)
 	conv_23 = BatchNormalization()(conv_23)
 	conv_23 = Activation('relu')(conv_23)
 
-	conv_24 = Conv2D(filters=64, kernel_size=(3,3), strides=(1, 1), padding='same')(conv_23)
+	conv_24 = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same')(conv_23)
 	conv_24 = BatchNormalization()(conv_24)
 	conv_24 = Activation('relu')(conv_24)
 
 	# Decoder block[1]
 	temp = UpSampling2D(size=(2, 2))(conv_24)
 
-	conv_25 = Conv2D(filters=64, kernel_size=(3,3), strides=(1, 1), padding='same')(temp)
+	conv_25 = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same')(temp)
 	conv_25 = BatchNormalization()(conv_25)
 	conv_25 = Activation('relu')(conv_25)
 
-
-	conv_26 = Conv2D(filters=nClasses, kernel_size=(1,1), strides=(1, 1), padding='valid')(conv_25)
+	conv_26 = Conv2D(filters=nClasses, kernel_size=(1, 1), strides=(1, 1), padding='valid')(conv_25)
 	conv_26 = BatchNormalization()(conv_26)
 
 	conv_26 = Reshape((input_shape[0] * input_shape[1], nClasses))(conv_26)
@@ -333,8 +333,3 @@ def segnet_level3(nClasses, input_height=500, input_width=750):
 	print("model.output_shape: ", model.output_shape, "\n")
 
 	return model
-
-
-
-
-
